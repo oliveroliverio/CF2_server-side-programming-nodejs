fd1b1d4b4f6b4e64fb2743421ffdf686da5055b6
oliveroliverio <oliveroliverio@gmail.com>
2025-06-18 16:20:28 -0700

    changed to correct port

--- movie_api/mongodb/server.js
+++ movie_api/mongodb/server.js
@@ -1,5 +1,5 @@
 const app = require('./index');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
