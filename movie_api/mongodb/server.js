f47ca716059d83cae9822a7979c88e68c2f14cc0
oliveroliverio <oliveroliverio@gmail.com>
2025-06-19 14:46:03 -0700

    port specified in .env file

--- movie_api/mongodb/server.js
+++ movie_api/mongodb/server.js
@@ -1,7 +1,7 @@
 const app = require('./index');
 const port = process.env.PORT || 8080;
 
-const server = app.listen(port, () => {
+const server = app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
 });
 
